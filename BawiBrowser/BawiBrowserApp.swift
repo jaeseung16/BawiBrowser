3d6a6d5e29fe973db49bab690fdc7ee7b490ef63
Jae-Seung Lee <jaeseung@gmail.com>
2021-06-28 17:35:53 -0400

    Initial Commit

---
+++ BawiBrowser/BawiBrowserApp.swift
@@ -0,0 +1,20 @@
+//
+//  BawiBrowserApp.swift
+//  BawiBrowser
+//
+//  Created by Jae Seung Lee on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BawiBrowserApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
