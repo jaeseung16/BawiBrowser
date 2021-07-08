e0011c62b1a778988893d805286e868d009c6660
Jae-Seung Lee <jaeseung@gmail.com>
2021-07-08 17:47:48 -0400

    Feature: Attach files
    TODO: Check the file's extension?

--- BawiBrowser/Views/WebView.swift
+++ BawiBrowser/Views/WebView.swift
@@ -214,5 +214,19 @@ struct WebView: NSViewRepresentable {
         func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
             print("runJavaScriptTextInputPanelWithPrompt: \(prompt)")
         }
+        
+        func webView(_ webView: WKWebView, runOpenPanelWith parameters: WKOpenPanelParameters, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping ([URL]?) -> Void) {
+            let openPanel = NSOpenPanel()
+            openPanel.canChooseFiles = true
+            openPanel.begin { result in
+                if result == NSApplication.ModalResponse.OK {
+                    if let url = openPanel.url {
+                        completionHandler([url])
+                    }
+                } else if result == NSApplication.ModalResponse.cancel {
+                    completionHandler(nil)
+                }
+            }
+        }
     }
 }
