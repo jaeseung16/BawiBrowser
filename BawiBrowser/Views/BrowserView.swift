d6a3633ca99ba180682019f4a74cfafdf836350f
Jae-Seung Lee <jaeseung@gmail.com>
2025-10-17 10:31:17 -0400

    feat: Move buttons to bottom

--- BawiBrowser/Views/BrowserView.swift
+++ BawiBrowser/Views/BrowserView.swift
@@ -18,87 +18,100 @@ struct BrowserView: View {
     @State private var linkCopied = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    viewModel.navigation = .back
-                }, label: {
-                    Image(systemName: "chevron.backward")
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    viewModel.navigation = .home
-                }, label: {
-                    Image(systemName: "house")
-                })
-                .keyboardShortcut("h", modifiers: [.command])
-                
-                Button(action: {
-                    viewModel.navigation = .logout
-                }, label: {
-                    Image(systemName: "rectangle.portrait.and.arrow.right")
-                })
-                .keyboardShortcut("h", modifiers: [.command])
-                
-                Spacer()
-                
-                Toggle(viewModel.isDarkMode ? "dark" : "light", isOn: $darkMode)
-                    .toggleStyle(SwitchToggleStyle())
-                    .onChange(of: darkMode) {
-                        viewModel.isDarkMode = darkMode
-                        viewModel.navigation = .reload
-                    }
-                
-                Button(action: {
-                    NSPasteboard.general.clearContents()
-                    NSPasteboard.general.setString(viewModel.didFinishURLString, forType: .string)
-                    linkCopied.toggle()
-                }, label: {
-                    Label("Copy Link", systemImage: "link")
-                })
-                .keyboardShortcut("l", modifiers: [.command])
-                
-                HStack {
-                    SearchView(searchString: $viewModel.searchString)
-                    
-                    if viewModel.searchResultTotalCount > 0 {
-                        Stepper {
-                            Text("\(viewModel.searchResultCounter) of \(viewModel.searchResultTotalCount)")
-                        } onIncrement: {
-                            viewModel.searchResultCounter += 1
-                            if viewModel.searchResultCounter >= viewModel.searchResultTotalCount {
-                                viewModel.searchResultCounter = viewModel.searchResultTotalCount
-                            }
-                        } onDecrement: {
-                            viewModel.searchResultCounter -= 1
-                            if viewModel.searchResultCounter <= 1 {
-                                viewModel.searchResultCounter = 1
-                            }
-                        }
-                    }
-                }
-                .frame(width: 250)
-                
-                Button(action: {
-                    viewModel.navigation = .forward
-                }, label: {
-                    Image(systemName: "chevron.forward")
-                })
-            }
-            .alert("Link Copied", isPresented: $linkCopied) {
-                Button("OK") {
-                    
-                }
-            } message: {
-                Text("\(viewModel.didFinishURLString)")
-            }
-            
+        ZStack {
             WebView(url: url)
                 .environmentObject(viewModel)
                 .shadow(color: Color.gray, radius: 1.0)
                 .colorScheme(viewModel.isDarkMode ? .dark : .light)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Button {
+                        viewModel.navigation = .back
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                    }
+                    .glassEffect()
+                    
+                    Spacer()
+                    
+                    Button {
+                        viewModel.navigation = .home
+                    } label: {
+                        Image(systemName: "house")
+                    }
+                    .keyboardShortcut("h", modifiers: [.command])
+                    .glassEffect()
+                    
+                    Button {
+                        viewModel.navigation = .logout
+                    } label: {
+                        Image(systemName: "rectangle.portrait.and.arrow.right")
+                    }
+                    .keyboardShortcut("h", modifiers: [.command])
+                    .glassEffect()
+                    
+                    Spacer()
+                    
+                    Toggle(viewModel.isDarkMode ? "dark" : "light", isOn: $darkMode)
+                        .toggleStyle(SwitchToggleStyle())
+                        .onChange(of: darkMode) {
+                            viewModel.isDarkMode = darkMode
+                            viewModel.navigation = .reload
+                        }
+                        .glassEffect()
+                    
+                    Button {
+                        NSPasteboard.general.clearContents()
+                        NSPasteboard.general.setString(viewModel.didFinishURLString, forType: .string)
+                        linkCopied.toggle()
+                    } label: {
+                        Label("Copy Link", systemImage: "link")
+                    }
+                    .keyboardShortcut("l", modifiers: [.command])
+                    .glassEffect()
+                    
+                    HStack {
+                        SearchView(searchString: $viewModel.searchString)
+                        
+                        if viewModel.searchResultTotalCount > 0 {
+                            Stepper {
+                                Text("\(viewModel.searchResultCounter) of \(viewModel.searchResultTotalCount)")
+                            } onIncrement: {
+                                viewModel.searchResultCounter += 1
+                                if viewModel.searchResultCounter >= viewModel.searchResultTotalCount {
+                                    viewModel.searchResultCounter = viewModel.searchResultTotalCount
+                                }
+                            } onDecrement: {
+                                viewModel.searchResultCounter -= 1
+                                if viewModel.searchResultCounter <= 1 {
+                                    viewModel.searchResultCounter = 1
+                                }
+                            }
+                            .glassEffect()
+                        }
+                    }
+                    .frame(width: 250)
+                    
+                    Button {
+                        viewModel.navigation = .forward
+                    } label: {
+                        Image(systemName: "chevron.forward")
+                    }
+                    .glassEffect()
+                }
+                .alert("Link Copied", isPresented: $linkCopied) {
+                    Button("OK") {
+                        
+                    }
+                } message: {
+                    Text("\(viewModel.didFinishURLString)")
+                }
+            }
+            
+            
         }
         .padding()
     }
